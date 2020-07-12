359519067862f7e942f3fba33ba2eeb93a983734
Viacheslav <brightsdayss@gmail.com>
2020-07-12 13:18:30 +0300

    post

--- js/script.js
+++ js/script.js
@@ -1,70 +1,91 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    
     function req() {
+        let formData = new FormData(form);
+        formData.append('id', Math.random());
+
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        //let json = JSON.stringify(obj);
+
         //const request = new XMLHttpRequest();
-        //request.open('GET', 'http://localhost:3000/people');
+        //request.open('POST', 'http://localhost:3000/people');
         //request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        //request.send();
+        //request.send(json);
         //request.addEventListener('load', function() {
         //    if (request.readyState === 4 && request.status == 200) {
         //        let data = JSON.parse(request.response);
         //        console.log(data);
-        //        createCards(data);
-        //        
+        //        //createCards(data);
         //    } else {
         //        console.error('error');
         //    }
         //});
 
-        getResource("http://localhost:3000/people")
-            .then(data => createCards(data.data))
-            .catch(err => console.error(err));
+        //getResource("http://localhost:3000/people", obj)
+        //    .then(data => createCards(data))
+        //    .catch(err => console.error(err));
 
-        this.remove();
+        axios.post("http://localhost:3000/people", obj);
+
+        //this.remove();
     }
 
-    document.querySelector('button').addEventListener('click', req, {'once': true});
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        req();
+    }, {'once': true});
 
-    //async function getResource(url) {
-    //    const res = await fetch(`${url}`);
-    //
-    //    if (!res.ok) {
-    //        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    //    }
-    //    
-    //    return await res.json();
-    //}
-
-    async function getResource(url) {
-        const res = await axios(`${url}`);
-
-        if (res.status !== 200) {
+    async function getResource(url, data) {
+        const res = await fetch(`${url}`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    
+        if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         
-        return res;
+        return await res.json();
     }
 
-    function createCards(response) {
-        response.forEach(item => {
-            let card = document.createElement('div');
-            card.classList.add('card');
+    //async function getResource(url) {
+    //    const res = await axios(`${url}`);
 
-            let icon;
-            if (item.sex === 'male') {
-                icon = "icons/mars.png";
-            } else {
-                icon = "icons/female.png";
-            }
+    //    if (res.status !== 200) {
+    //        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //    }
+        
+    //    return res;
+    //}
 
-            card.innerHTML = `
-                <img src="${item.photo}" alt="">
-                <div class="name">${item.name} ${item.surname}</div>
-                <div class="sex">
-                    <img src=${icon} alt="male">
-                </div>
-                <div class="age">${item.age}</div>
-            `;
-            document.querySelector('.app').appendChild(card);
-        });
-    }
+    //function createCards(response) {
+    //    response.forEach(item => {
+    //        let card = document.createElement('div');
+    //        card.classList.add('card');
+
+    //        let icon;
+    //        if (item.sex === 'male') {
+    //            icon = "icons/mars.png";
+    //        } else {
+    //            icon = "icons/female.png";
+    //        }
+
+    //        card.innerHTML = `
+    //            <img src="${item.photo}" alt="">
+    //            <div class="name">${item.name} ${item.surname}</div>
+    //            <div class="sex">
+    //                <img src=${icon} alt="male">
+    //            </div>
+    //            <div class="age">${item.age}</div>
+    //        `;
+    //        document.querySelector('.app').appendChild(card);
+    //    });
+    //}
 });
